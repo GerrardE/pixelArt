e869810ed97d2d5ffb8c55675d70a0949d6cd487
GerrardE <ezeugwagerrard@gmail.com>
2018-06-30 13:06:13 +0100

    5th commit

--- designs.js
+++ designs.js
@@ -10,27 +10,27 @@ $(document).ready(function(){
 			//clear the cells on each submission
 			$('table td, tr').remove();
 				//collect the form ids and declare their values as variables
-				var rows = $('#inputWidth').val();
-				var cols = $('#inputHeight').val();
+				const rows = $('#inputWidth').val();
+				const cols = $('#inputHeight').val();
 				//loop through the value of rows and display a row each time
-				for (var x = 1; x<=rows; x++){
+				for (let x = 1; x<=rows; x++){
 						$('table').append('<tr></tr>');
 					}
 				//loop through the value of cols and display a col each time
-				for (var y = 1; y<=cols; y++){
+				for (let y = 1; y<=cols; y++){
 						$('tr').append('<td></td>');
 						//give a class of 'cells' to each table data 
 						$('td').attr('class', 'cells');
 					}
 				//color the cells on click with the event listner
-				$('.cells').click(function(color){
+				$('.cells').click(function(event){
 					//collect the color value selection
-					var paint = $('#colorPicker').val();
+					const paint = $('#colorPicker').val();
 					//color the target cell
 					if ($(this).attr('style')){	
 						$(this).removeAttr('style');
 							} else {
-								$(color.target).css('background-color', paint);	
+								$(event.target).css('background-color', paint);	
 						}
 				});		
 		});
